2fbacb41e708356a246bcbd0a4af02b286b1defb
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-11-16 20:49:29 +1300

    Count uploads

--- lbry/extras/daemon/data_stats.py
+++ lbry/extras/daemon/data_stats.py
@@ -1,4 +1,5 @@
 import apsw
+import datetime
 import time
 
 # TODO: Use conf data_dir
@@ -28,9 +29,17 @@ class DataStats:
 
         self.db.execute("""
             CREATE TABLE IF NOT EXISTS blob
-            (blob_hash         TEXT NOT NULL PRIMARY KEY,
-             count_seeds       INTEGER NOT NULL DEFAULT 0,
-             last_seeded_epoch INTEGER)
+            (blob_hash      TEXT NOT NULL PRIMARY KEY,
+             seed_count     INTEGER NOT NULL,
+             last_seed_time REAL NOT NULL)
+            WITHOUT ROWID;""")
+
+
+        self.db.execute("""
+            CREATE TABLE IF NOT EXISTS hour
+            (start_time INTEGER NOT NULL PRIMARY KEY,
+             blobs_up   INTEGER NOT NULL,
+             blobs_down INTEGER NOT NULL)
             WITHOUT ROWID;""")
 
         self.db.execute("COMMIT;")
@@ -39,17 +48,32 @@ class DataStats:
     def save(self):
 
         self.db.execute("BEGIN;")
+
         for item in self.seeded:
+
+            blob_hash, epoch = item
 
             # Update seed counts and last seed time for the blob
             self.db.execute("""
                 INSERT INTO blob VALUES (?, 1, ?)
                 ON CONFLICT (blob_hash)
                 DO UPDATE SET
-                    count_seeds = count_seeds + 1,
-                    last_seeded_epoch = excluded.last_seeded_epoch;""",
-                item)
+                    seed_count = seed_count + 1,
+                    last_seed_time = excluded.last_seed_time;""",
+                (blob_hash, epoch))
+
+            # Update hour table
+            hour = datetime.datetime.fromtimestamp(epoch)
+            hour = hour.replace(minute=0, second=0, microsecond=0)
+            self.db.execute("""
+                INSERT INTO hour VALUES (?, 1, 0)
+                ON CONFLICT (start_time)
+                DO UPDATE SET blobs_up = blobs_up + 1;
+                """, (int(hour.timestamp()), ))
+
         self.db.execute("COMMIT;")
+
+        # Clear in-memory data
         self.seeded = []
 
 
@@ -58,7 +82,7 @@ class DataStats:
         Log that a particular blob was seeded.
         """
         now = time.time()
-        self.seeded.append((blob_hash, int(now)))
+        self.seeded.append((blob_hash, now))
 
         if len(self.seeded) >= 100 or \
             (len(self.seeded) > 0 and now - self.seeded[0][1] >= 3600):
